a93bfe42b3981f96944a08ad37ff50bd35c147dc
Rahulk2202 <rahuljcet95@gmail.com>
2024-07-19 14:06:59 +0530

    initial commit

---
+++ event_desk/event_desk/doctype/event_settings/event_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Anther Technologies and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class EventSettings(Document):
+	pass
